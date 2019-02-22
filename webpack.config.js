6110db06bbe251c69caa9d102913d0951d4146f2
Josh McKenney <joshmckenney@gmail.com>
2019-02-22 07:27:48 -0800

    hi google?

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,6 @@ const path = require('path');
 const defaultConfig = require('@open-wc/building-webpack/default-config');
 
 module.exports = defaultConfig({
-  indexHTML: path.resolve(__dirname, './src/googlef9901a4167e4ea1e.html'),
+  indexHTML: path.resolve(__dirname, './src/index.html'),
   indexJS: path.resolve(__dirname, './src/index.js'),
 });
