a029b527bb961d316bf9471cd5108f72231f1789
jmckenney <joshmckenney@gmail.com>
2019-07-11 08:49:27 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 // eslint-disable-next-line import/no-extraneous-dependencies
-const defaultConfig = require('@open-wc/building-webpack/default-config');
+// const defaultConfig = require('@open-wc/building-webpack/default-config');
+const defaultConfig = require('@open-wc/building-webpack/modern-and-legacy-config');
 
 module.exports = defaultConfig({
   indexHTML: path.resolve(__dirname, './src/index.html'),
